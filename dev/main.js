e492780f691b27f2175928508072e9ebfed081e8
Kaxaru <khidechi@gmail.com>
2020-03-22 06:39:41 +0300

    feat: add ru vocabulary

--- dev/main.js
+++ dev/main.js
@@ -1,18 +1,19 @@
 const keyboard = document.querySelector('.keyboard')
 keyboard.innerHTML = "";
-const LANG = "en"
 const LANGKEYS = [16, 20]
 const BLOCKPRINTKEYS = [17, 18]
+let lang
 
-init = () => {
+let init = () => {
+    lang = localStorage.getItem('lang') || "en"
     fetch("../dev/keyboard.json").then(res => res.json()).then(data => {
-        let sections = Object.keys(data[LANG])
+        let sections = Object.keys(data["Lang"][lang])
         let template = ""
         sections.map(s => {
             let templSection = ""
-            let keys = Object.keys(data[LANG][s])
+            let keys = Object.keys(data["Lang"][lang][s])
             keys.map(k => {
-                let key = data[LANG][s][k]
+                let key = data["Lang"][lang][s][k]
                 templSection += `<div 
                     class="${key.class.split(",").join(" ")}" 
                     data-key="${key.datakey}"
@@ -22,6 +23,7 @@ init = () => {
             })
             template += `<section class=${s}>${templSection}</section>`
         })
+        localStorage.setItem('lang', lang)
         keyboard.innerHTML = template
 
         const keys =  Array.from(keyboard.querySelectorAll('.key'))
@@ -38,9 +40,7 @@ init = () => {
                     key.classList.contains('active') ? key.classList.remove('active') : key.classList.add('active')
                 }
 
-                if (!key.classList.contains('system')) {
-                    output.value += key.textContent
-                }
+                key.classList.contains('system') ? systemKeyHandling(key, e.type, e.repeat) : keyHandling(key) 
             })
 
             window.addEventListener('mouseup', (e) => {
@@ -50,40 +50,49 @@ init = () => {
                         key.classList.remove('active')
                     }
                 })
+                key.classList.contains('system') ? systemKeyHandling(key, e.type, e.repeat) : null; 
                 keyUpNow = []
             })
         })
     })   
 }
 
-/*refresh = (btn) => {
+let refresh = () => {
+    lang = localStorage.getItem('lang') == "en" ? "ru" : "en"
+    localStorage.setItem('lang', lang)
+    let htmlToChar = (string) => {
+        return /&#/.test(string) ? String.fromCharCode(string.replace("&#", "")) : string
+    }
     fetch("../dev/keyboard.json").then(res => res.json()).then(data => {   
-        let sections = Object.keys(data[LANG])
+        let sections = Object.keys(data["Lang"][lang])
         sections.map(s => {
-            let keys = Object.keys(data[LANG][s])
+            let keys = Object.keys(data["Lang"][lang][s])
+            keys.map(k => {
+                let key = data["Lang"][lang][s][k]
+                if(!/system/i.test(key.class)) {
+                    let element = document.querySelector(`.key[data-key="${key.datakey}"]`)
+                    element.setAttribute("data-downreg", htmlToChar(key.dReg))
+                    element.setAttribute("data-upreg", htmlToChar(key.upReg))
+                    element.textContent = htmlToChar(key.upReg)
+                }
+            })
         })
     })
-}*/
+}
 
 init()
 
-changeRegister = (active = true) => {
-    if (active) {
-        let upReg = Array.from(document.querySelectorAll('.key[data-upreg]'))
-        upReg.map(key => {
-           key.textContent = key.getAttribute('data-upreg')
-        })
-    } else {
-        let downReg = Array.from(document.querySelectorAll('.key[data-downreg]'))
-        downReg.map(key => {
-            key.textContent = key.getAttribute('data-downreg')
-        })
-    }
+let changeRegister = (active = true) => {
+    let attr = (active) ? 'data-upreg' : 'data-downreg'
+    let els = Array.from(document.querySelectorAll(`.key[${attr}]`))
+    els.map(key => {
+        key.textContent = key.getAttribute(attr)
+    })
 }
 
 const output = document.querySelector('.output')
-systemKeyHandling = (key, event) => {
-    if(event == "keyup") {
+let systemKeyHandling = (key, event, eventRepeat = false) => {
+    if(event == "keyup" || event == "mouseup") {
         let checkBtn = false
         LANGKEYS.map(langkey => {
             if(langkey == +key.getAttribute('data-key'))
@@ -125,14 +134,15 @@ systemKeyHandling = (key, event) => {
     }
 
     isActiveShiftOrCapsLock = () => {
-        let CapsLock = document.querySelectorAll('.key[data-key="20"]')
-        let Shift = document.querySelectorAll('.key[data-key="16"]')
-        let btns = [...CapsLock, ...Shift]
-        for(let i = 0; i < btns.length; i++) {
-            if(btns[i].classList.contains('active'))
+        let capitalLetterEls = []
+        LANGKEYS.map(k => {
+            capitalLetterEls = [...capitalLetterEls, ...document.querySelectorAll(`.key[data-key="${k}"]`)]
+        })
+
+        for(let i = 0; i < capitalLetterEls.length; i++) {
+            if(capitalLetterEls[i].classList.contains('active'))
                 return true
         }
-
         return false
     }
 
@@ -142,22 +152,34 @@ systemKeyHandling = (key, event) => {
         case "9":
             text = [...output.value]
             cursorPos = getCursorPos(output)
-            text.map((c, ind) => {
-                newtext.push(c)
-                if (ind + 1 == cursorPos) {
-                    newtext.push("  ")
-                }
-            })
-            text = newtext.join("")
-            newtext = []
-            output.value = text
-            setCursorPos(output, cursorPos + 2)
+            if(text.length != 0) {
+                text.map((c, ind) => {
+                    newtext.push(c)
+                    if (ind + 1 == cursorPos) {
+                        newtext.push("  ")
+                    }
+                })
+                text = newtext.join("")
+                newtext = []
+                output.value = text
+                setCursorPos(output, cursorPos + 2)
+            } else {
+                output.value = "  "
+            }
+            
             break;
         case "8":
             text = [...output.value]
-            cursorPos = getCursorPos(output)
-            output.value = [...text.slice(0, cursorPos - 1), ...text.slice(cursorPos)].join("")
-            setCursorPos(output, cursorPos - 1)
+            let selection = document.getSelection().toString()
+            if (!!selection) {
+                cursorPos = getCursorPos(output)
+                output.value = output.value.replace(selection, "")
+                setCursorPos(output, cursorPos )
+            } else {
+                cursorPos = getCursorPos(output)
+                output.value = [...text.slice(0, cursorPos - 1), ...text.slice(cursorPos)].join("")
+                setCursorPos(output, cursorPos - 1)
+            }
             break;
         case "37":
             cursorPos = getCursorPos(output)
@@ -212,6 +234,14 @@ systemKeyHandling = (key, event) => {
             setCursorPos(output, searchNewLine(text, cursorPos - 1, true)) 
             break;
         case "16":
+            if(!eventRepeat && event == "keydown") {
+                if (key.getAttribute('data-direction') == "left") {
+                    let leftAlt = document.querySelector('.LAlt')
+                    if (leftAlt.classList.contains('active') && document.querySelectorAll('.system.active').length == 2) {
+                        refresh()
+                    }
+                } 
+            } 
             changeRegister(isActiveShiftOrCapsLock())
             break;
         case "20":
@@ -220,22 +250,30 @@ systemKeyHandling = (key, event) => {
         case "17":
             break;
         case "18":
+            if(!eventRepeat && event == "keydown") {
+                if (key.getAttribute('data-direction') == "left") {
+                    let leftShift = document.querySelector('.LShift')
+                    if (leftShift.classList.contains('active') && document.querySelectorAll('.system.active').length == 2) {
+                        refresh()
+                    }
+                } 
+            } 
             break;
     }
 }
 
-keyHandling = (key) => {
+let keyHandling = (key) => {
     let cursorPos, text
     let newtext = []
 
-    getCursorPos = (obj) => {
+    let getCursorPos = (obj) => {
         obj.focus()
         if(obj.selectionStart) 
             return obj.selectionStart
         return 0
     }
 
-    setCursorPos = (obj, position, direction = false) => {
+    let setCursorPos = (obj, position, direction = false) => {
         if (direction) {
             obj.selectionStart = position
         } else {
@@ -276,9 +314,18 @@ keyHandling = (key) => {
     }
 }
 
+
 window.addEventListener('keydown', (e) => {
     e.preventDefault()
-    const key = document.querySelector(`div[data-key="${e.keyCode}"]`)
+    let key
+    if (e.keyCode == 17 || e.keyCode == 18 || e.keyCode == 16) {
+        key = e.location == 1 
+                ? document.querySelector(`.L${e.key}`)
+                : document.querySelector(`.R${e.key}`)
+
+    } else {
+        key = document.querySelector(`div[data-key="${e.keyCode}"]`)
+    }
     
     if (e.keyCode != 20) {
         key.classList.add('active')
@@ -286,17 +333,25 @@ window.addEventListener('keydown', (e) => {
         key.classList.contains('active') ? key.classList.remove('active') : key.classList.add('active')
     }
     
-    key.classList.contains('system') ? systemKeyHandling(key, e.type) : keyHandling(key) 
+    key.classList.contains('system') ? systemKeyHandling(key, e.type, e.repeat) : keyHandling(key) 
     
 })
 
 window.addEventListener('keyup', (e) => {
     e.preventDefault()
-    const key = document.querySelector(`div[data-key="${e.keyCode}"]`)
+    let key
+    if (e.keyCode == 17 || e.keyCode == 18 || e.keyCode == 16) {
+        key = e.location == 1 
+                ? document.querySelector(`.L${e.key}`)
+                : document.querySelector(`.R${e.key}`)
+
+    } else {
+        key = document.querySelector(`div[data-key="${e.keyCode}"]`)
+    }
     if( e.keyCode != 20) {
         key.classList.remove('active')
     }
-    key.classList.contains('system') ? systemKeyHandling(key, e.type) : null; 
+    key.classList.contains('system') ? systemKeyHandling(key, e.type, e.repeat) : null; 
 } )
 
 
